df2b411bcbc9d5aaaf1e63fc7f770de3bbe0d250
ViniciusProgrammer <viniciusimdufrn@gmail.com>
2026-02-23 15:13:57 -0300

    adicionada a funcionalidade que mostra a saudação de acordo com o horário do dia

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -9,8 +9,28 @@ func Hello(name string) string {
 	if name == "" {
 		name = "Mundo"
 	}
-	
+
 	return prefixoPortugues + name
+}
+
+// Função de visibilidade privada quando começa com letra caixa baixa
+func getTimeGreeting(hour int) string {
+	if hour >= 5 && hour < 12 {
+		return "Bom dia"
+	} else if hour >= 12 && hour < 18 {
+		return "Boa Tarde"
+	}
+
+	return "Boa Noite"
+}
+
+// Função de visibilidade pública quando começa com letra caixa alta
+func HelloWithTime(name string, hour int) string {
+	if name == "" {
+		name = "Mundo"
+	}
+
+	return getTimeGreeting(hour) + ", " + name
 }
 
 // efeito colateral
--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -14,8 +14,8 @@ func assertMessage(t testing.TB, got string, want string) {
 func TestHello(t *testing.T) {
 	// sub-teste para retorno da função que recebe uma string não vazia
 	t.Run("cumprimentar pessoas", func(t *testing.T) {
-		got := Hello("Web II")
-		want := "Olá, Web II"
+		got := Hello("Maria")
+		want := "Olá, Maria"
 
 		assertMessage(t, got, want)
 	})
@@ -24,6 +24,27 @@ func TestHello(t *testing.T) {
 		got := Hello("")
 		want := "Olá, Mundo"
 
+		assertMessage(t, got, want)
+	})
+
+	t.Run("saudação de manhã", func(t *testing.T) {
+		hour := 9
+		got := HelloWithTime("Rodolfo", hour)
+		want := "Bom dia, Rodolfo"
+		assertMessage(t, got, want)
+	})
+
+	t.Run("saudação de tarde", func(t *testing.T) {
+		hour := 16
+		got := HelloWithTime("Maria", hour)
+		want := "Boa Tarde, Maria"
+		assertMessage(t, got, want)
+	})
+
+	t.Run("saudação da noite", func(t *testing.T) {
+		hour := 21
+		got := HelloWithTime("Pedro", hour)
+		want := "Boa Noite, Pedro"
 		assertMessage(t, got, want)
 	})
 }
