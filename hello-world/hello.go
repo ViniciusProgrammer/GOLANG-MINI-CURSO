2912435549f05ca3f627ed04f014a724e887a373
ViniciusProgrammer <viniciusimdufrn@gmail.com>
2026-02-23 19:20:19 -0300

    feat: implementada a funcionalidade de saudação pelo  vocabulario de  estados do Brasil

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -4,6 +4,39 @@ import "fmt"
 
 const prefixoPortugues = "Olá, "
 
+const(
+	RegionRN = "rn"
+	RegionSP = "sp"
+	RegionMG = "mg"
+	RegionRS = "rs"
+)
+
+func getRegionalVocative(region string) string {
+	switch region {
+	case RegionRN:
+			return "boy"
+	case RegionSP:
+		return "mano"
+	case RegionMG:
+		return "sô"
+	case RegionRS:
+		return "tchê"
+	default:
+		return "" // sem vocativo regional
+	} 	
+}
+
+func HelloWithRegion(hour int, region string) string {
+	greeting := getTimeGreeting(hour)
+	vocative := getRegionalVocative(region)
+
+	if vocative != "" {
+		return greeting + ", " + vocative
+	}
+
+	return greeting + ", Mundo"
+}
+
 // dominio
 func Hello(name string) string {
 	if name == "" {
@@ -16,7 +49,7 @@ func Hello(name string) string {
 // Função de visibilidade privada quando começa com letra caixa baixa
 func getTimeGreeting(hour int) string {
 	if hour >= 5 && hour < 12 {
-		return "Bom dia"
+		return "Bom Dia"
 	} else if hour >= 12 && hour < 18 {
 		return "Boa Tarde"
 	}
--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -10,6 +10,32 @@ func assertMessage(t testing.TB, got string, want string) {
 	}
 }
 
+func TestHelloWithRegion(t * testing.T) {
+	t.Run("Rio Grande do Norte morning", func(t *testing.T) {
+		got := HelloWithRegion(9, RegionRN)
+		want := "Bom Dia, boy"
+		assertMessage(t, got, want)
+	})
+
+	t.Run("São Paulo afternoon", func(t *testing.T) {
+		got := HelloWithRegion(14, RegionSP)
+		want := "Boa Tarde, mano"
+		assertMessage(t, got, want)
+	})
+
+	t.Run("Minas Gerais morning", func(t *testing.T) {
+		got := HelloWithRegion(10, RegionMG)
+		want := "Bom Dia, sô"
+		assertMessage(t, got, want)
+	})
+
+	t.Run("Rio Grande do Sul evening", func(t *testing.T) {
+		got := HelloWithRegion(22, RegionRS)
+		want := "Boa Noite, tchê"
+		assertMessage(t, got, want)
+	})
+}
+
 // Função de teste
 func TestHello(t *testing.T) {
 	// sub-teste para retorno da função que recebe uma string não vazia
@@ -30,7 +56,7 @@ func TestHello(t *testing.T) {
 	t.Run("saudação de manhã", func(t *testing.T) {
 		hour := 9
 		got := HelloWithTime("Rodolfo", hour)
-		want := "Bom dia, Rodolfo"
+		want := "Bom Dia, Rodolfo"
 		assertMessage(t, got, want)
 	})
 
